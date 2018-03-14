f4cefcdcb900a42d0ce33687d47045b55f84057e
André Formento <andreformento.sc@gmail.com>
2018-03-13 21:19:46 -0300

    First commit

---
+++ search-pipeline/src/main/java/com/formento/search/pipeline/SearchPipelineApplication.java
@@ -0,0 +1,12 @@
+package com.formento.search.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPipelineApplication.class, args);
+	}
+}
