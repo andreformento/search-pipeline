26ad750b0a7027cf7afd77b09a04e0dfa8a21c51
André Formento <andreformento.sc@gmail.com>
2018-03-26 19:46:18 -0300

    Funcional list ready

--- search-pipeline/src/main/java/com/formento/search/pipeline/core/FunctionalList.java
+++ search-pipeline/src/main/java/com/formento/search/pipeline/core/FunctionalList.java
@@ -3,17 +3,17 @@ package com.formento.search.pipeline.core;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
-public class FunctionalList<T> {
+public final class FunctionalList<T> {
 
-    private final LinkedList<T> list;
+    private final LinkedList<Function<T, T>> list;
 
-    public FunctionalList(final List<T> list) {
+    public FunctionalList(final List<Function<T, T>> list) {
         this.list = new LinkedList<>(list);
     }
 
-
-    public Optional<T> head() {
+    public Optional<Function<T, T>> head() {
         if (list.isEmpty()) {
             return Optional.empty();
         } else {
@@ -27,6 +27,22 @@ public class FunctionalList<T> {
         } else {
             return Optional.of(new FunctionalList<>(list.subList(1, list.size())));
         }
+    }
+
+    public T consume(T value) {
+        return consumeRecursive(this, value);
+    }
+
+    private T consumeRecursive(FunctionalList<T> functionalList, T value) {
+        final T nextValue = functionalList.
+                head().
+                map(action -> action.apply(value)).
+                orElse(value);
+
+        return functionalList.
+                tail().
+                map(tailList -> consumeRecursive(tailList, nextValue)).
+                orElse(nextValue);
     }
 
 }
--- search-pipeline/src/test/java/com/formento/search/pipeline/core/FunctionalListTest.java
+++ search-pipeline/src/test/java/com/formento/search/pipeline/core/FunctionalListTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,26 +14,38 @@ public class FunctionalListTest {
 
     @Test
     public void shouldGetHeadFromEmptyList() {
-        final List<Object> list = Collections.emptyList();
+        final List<Function<Object, Object>> list = Collections.emptyList();
         assertThat(new FunctionalList<>(list).head()).isEmpty();
     }
 
     @Test
     public void shouldGetHeadFromList() {
-        final List<Object> list = ImmutableList.builder().add(new Object()).build();
+        final List<Function<Object, Object>> list = ImmutableList.<Function<Object, Object>>builder().add(o -> o).build();
         assertThat(new FunctionalList<>(list).head()).isNotEmpty();
     }
 
     @Test
     public void shouldGetTailFromEmptyList() {
-        final List<Object> list = Collections.emptyList();
+        final List<Function<Object, Object>> list = Collections.emptyList();
         assertThat(new FunctionalList<>(list).tail()).isEmpty();
     }
 
     @Test
     public void shouldGetTailFromList() {
-        final List<Object> list = ImmutableList.builder().add(new Object(), new Object()).build();
+        final List<Function<Object, Object>> list = ImmutableList.<Function<Object, Object>>builder().add(o -> o, o -> o).build();
         assertThat(new FunctionalList<>(list).tail()).isNotEmpty();
+    }
+
+    @Test
+    public void shouldApplyTransformationsInValue() {
+        final List<Function<String, String>> transformations =
+                ImmutableList.<Function<String, String>>builder().add(
+                        String::trim,
+                        String::toLowerCase
+                ).build();
+        final FunctionalList<String> functionalList = new FunctionalList<>(transformations);
+
+        assertThat(functionalList.consume(" André Formento ")).isEqualTo("andré formento");
     }
 
 }
